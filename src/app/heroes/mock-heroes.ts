aeff3aa478cc605423a5edc2d16b4311df80dfde
TomkaYurii <tomka.yurii@gmail.com>
2023-04-26 18:03:45 +0300

    first

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. 11' },
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bomba' },
+  { id: 14, name: 'CtrlAltDel' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RMan' },
+  { id: 17, name: 'Mama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tada' }
+];
